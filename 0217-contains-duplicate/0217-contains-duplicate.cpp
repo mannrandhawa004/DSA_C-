0eb550fc667059ade106f1618ad64c1fe6999f32
Manpreet Singh <mannrandhawa004@gmail.com>
2026-07-08 21:49:40 +0530

    Time: 75 ms (18.53%), Space: 111.2 MB (13.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> mp;
-
-        for(int num : nums){
-            if(mp.count(num) > 0){
-                return true;
-            }
-            mp.insert(num);
+         unordered_set<int> seen;
+    for (int num : nums) {
+        if (seen.count(num)) {
+            return true;
         }
-        return false;
+        seen.insert(num);
+    }
+    return false;
     }
 };
