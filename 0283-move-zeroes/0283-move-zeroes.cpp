9d441939b632c56bbe055691fd679006a7584ca5
Manpreet Singh <mannrandhawa004@gmail.com>
2026-07-20 21:01:48 +0530

    Time: 1 ms (17.72%), Space: 24 MB (19.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,9 +3,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int insertPos = 0;
 
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                swap(nums[i], nums[insertPos]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                if (i != insertPos) {
+                    swap(nums[i], nums[insertPos]);
+                }
                 insertPos++;
             }
         }
