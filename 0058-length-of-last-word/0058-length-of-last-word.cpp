0ddd086e5db099fab1e74a8f93656f26ec0ea1f5
Manpreet Singh <mannrandhawa004@gmail.com>
2026-07-23 22:12:55 +0530

    Time: 3 ms (2.23%), Space: 9.9 MB (5.12%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        string last;
+
+        while( ss >> word){
+            last = word;
+        }
+
+        return last.size();
+    }
+};
