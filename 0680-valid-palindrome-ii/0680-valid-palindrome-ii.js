7fc1d0ea295e43d78d08eeeb7d587fe9b4d46a6f
Manpreet Singh <mannrandhawa004@gmail.com>
2026-08-08 18:22:20 +0530

    Time: 10 ms (20%), Space: 61 MB (58.91%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return isPlaindrome(s, left + 1, right) || isPlaindrome(s, left, right - 1);
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+};
+
+const isPlaindrome = (s, left, right) => {
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
